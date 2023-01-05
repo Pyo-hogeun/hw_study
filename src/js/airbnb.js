cfdda1e4c2d5404f5cec0799289b3f3712d02d9d
HyungwonSuh <0526shw@gmail.com>
2023-01-05 16:57:20 +0900

    main페이지 filter작업중

--- src/js/airbnb.js
+++ src/js/airbnb.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-
+//airbnb-main start--------------------------------------------
     var optionImageArr = document.querySelectorAll('.option-image');
     for(var i = 0; i < optionImageArr.length; i++){
         optionImageArr[i].style.backgroundImage = 
         `url('./src/img/airbnb/option_bar_${i}.png')`
     };
 
+    $('.filter-icon').click(function(){
+        
+    })
+//airbnb-main end--------------------------------------------
+
+//airbnb-wishlist start--------------------------------------------
     $('.login-button').click(function(){
         $('.load-login').css('display', 'block')
         $('.load-login').animate({'margin-top': '0'})
@@ -16,11 +22,14 @@ $(document).ready(function(){
         $('.load-login').animate({'margin-top': '700'})
         $('.airbnb-wishlist .dim-class').removeClass('dim')
     })
+//airbnb-wishlist end--------------------------------------------
 
+//airbnb-login start--------------------------------------------
     $('.nation-select').change(function(){
         let nationSelected = $('.nation-select option:selected').text()
         $('.input-number').attr('value', nationSelected.trim().split('(')[1].split(')')[0])
     })
+//airbnb-login end--------------------------------------------
 
    
 
