672cf142235c862e03ede7e6fedd1e24f16fad37
HyungwonSuh <0526shw@gmail.com>
2023-01-05 15:17:40 +0900

    wishlist / css,html 완성

--- src/js/airbnb.js
+++ src/js/airbnb.js
@@ -1,18 +1,46 @@
-var optionImageArr = document.querySelectorAll('.option-image');
-for(var i = 0; i < optionImageArr.length; i++){
-    optionImageArr[i].style.backgroundImage = `url('./src/img/airbnb/option_bar_${i}.png')`
-}
+$(document).ready(function(){
 
-var bottomImgArr = document.querySelectorAll('.bottom-img');
-bottomImgArr[0].addEventListener('click', function(){
-    location.href='./airbnb-main.html'
-})
-bottomImgArr[1].addEventListener('click', function(){
-    location.href='./airbnb-wishlist.html'
-})
-bottomImgArr[2].addEventListener('click', function(){
-    location.href='./airbnb-login.html'
-})
+    var optionImageArr = document.querySelectorAll('.option-image');
+    for(var i = 0; i < optionImageArr.length; i++){
+        optionImageArr[i].style.backgroundImage = 
+        `url('./src/img/airbnb/option_bar_${i}.png')`
+    };
+
+    $('.login-button').click(function(){
+        $('.load-login').css('display', 'block')
+        $('.load-login').animate({'margin-top': '0'})
+        $('.airbnb-wishlist .dim-class').addClass('dim')
+    });
+    $('.load-login .xbutton').click(function(){
+        $('.load-login').css('display', 'none')
+        $('.load-login').animate({'margin-top': '700'})
+        $('.airbnb-wishlist .dim-class').removeClass('dim')
+    })
+
+    $('.nation-select').change(function(){
+        let nationSelected = $('.nation-select option:selected').text()
+        $('.input-number').attr('value', nationSelected.trim().split('(')[1].split(')')[0])
+    })
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
 
 
