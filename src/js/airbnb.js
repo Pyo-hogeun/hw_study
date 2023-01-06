3ef2e3848fc6cc81f5204b48db3461bd52bd1f5b
hw <0526shw@gmail.com>
2023-01-06 14:29:39 +0900

    wishlish, login페이지 / html, css 완성

--- src/js/airbnb.js
+++ src/js/airbnb.js
@@ -14,13 +14,16 @@ $(document).ready(function(){
 //airbnb-wishlist start--------------------------------------------
     $('.login-button').click(function(){
         $('.load-login').css('display', 'block')
-        $('.load-login').animate({'margin-top': '0'})
+        $('.load-login').animate({'margin-top': '-80px'})
         $('.airbnb-wishlist .dim-class').addClass('dim')
+        $('.gnb-bottom').addClass('hide')
+        
     });
     $('.load-login .xbutton').click(function(){
         $('.load-login').css('display', 'none')
         $('.load-login').animate({'margin-top': '700'})
         $('.airbnb-wishlist .dim-class').removeClass('dim')
+        $('.gnb-bottom').removeClass('hide')
     })
 //airbnb-wishlist end--------------------------------------------
 
@@ -28,6 +31,20 @@ $(document).ready(function(){
     $('.nation-select').change(function(){
         let nationSelected = $('.nation-select option:selected').text()
         $('.input-number').attr('value', nationSelected.trim().split('(')[1].split(')')[0])
+    })
+
+    $('.email').click(function(){
+        $('.email-input').toggleClass('hide')
+        $('.user-number').toggleClass('hide')
+        $('.warning').toggleClass('hide')
+        $('.loginwith-number').toggleClass('hide')
+        $('.loginwith-email').toggleClass('hide')
+    })
+
+
+    $('.apple-popup .drop-down-button').click(function(){
+        $('.hide').toggleClass('show-hide');
+        $('.dim').toggleClass('show-hide');
     })
 //airbnb-login end--------------------------------------------
 
